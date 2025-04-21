9975ae5b947419c5501889b3ffe44d36ebbf985c
Rizky Fitriyanto <rizkyfitriyanto678@gmail.com>
2025-04-21 18:05:37 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from assignment1_city_astar import run_assignment1
+from assignment2_ambulance_gbfs import run_assignment2
+from assignment3_food_astar import run_assignment3
+from assignment4_drone_astar import run_assignment4
+
+def main():
+    run_assignment1()
+    run_assignment2()
+    run_assignment3()
+    run_assignment4()
+
+if __name__ == "__main__":
+    main()
